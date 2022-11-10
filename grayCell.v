0a801154d91383362535bf5fb9b65ac803a36a4a
HimanshuPandey09 <114841870+HimanshuPandey09@users.noreply.github.com>
2022-11-10 09:15:21 +0530

    Delete grayCell.v

--- grayCell.v
+++
@@ -1,9 +0,0 @@
-`timescale 1ns / 1ps
-
-module grayCell(output out,
-                input a,
-                input b,
-                input c);
-                
-assign out = a | b & c;               
-endmodule
